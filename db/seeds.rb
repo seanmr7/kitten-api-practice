f3ecf6ff3830527efffbaea4b87e9cbbb4f8c262
Sean <smriedl@gmail.com>
2021-02-13 13:16:42 -0500

    Implement json response in controller for index and show actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Kitten.new( {name: "Steve", age: "10", cuteness: "3", softness: "10" } ).save
+Kitten.new( {name: "Emily", age: "6", cuteness: "10", softness: "2" } ).save
+Kitten.new( {name: "Joe", age: "22", cuteness: "7", softness: "7" } ).save
+Kitten.new( {name: "Poutine", age: "5", cuteness: "1", softness: "3" } ).save
+Kitten.new( {name: "Cat", age: "1", cuteness: "10", softness: "10" } ).save
